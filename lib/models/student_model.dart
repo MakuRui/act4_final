756e0c9899c5cfb335c75f62ef224c8fc3000c6b
MakuRui <marklouienambatac@gmail.com>
2022-10-31 21:51:20 +0800

    Mark Commit it's duty

--- lib/models/student_model.dart
+++ lib/models/student_model.dart
@@ -1,6 +1,6 @@
 class StudentModel {
-  final String iD, name, birthday, gender, yearSec, course, MakuRui;
+  final String iD, name, birthday, gender, yearSec, course;
 
   StudentModel(this.iD, this.name, this.birthday, this.gender, this.yearSec,
-      this.course, this.MakuRui);
+      this.course);
 }
